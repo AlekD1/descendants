b4bdee5c4e5cf955612eb6045f06150e72ce5eb0
Alexei Doloban <alekdn.work@yandex.ru>
2024-01-31 16:34:32 +0300

    ready12

--- SevDP/settings.py
+++ SevDP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@$%eiyl737=hkasddaw%+w)9v-dmyqsqd7=&bo%exq9m(%cawx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
