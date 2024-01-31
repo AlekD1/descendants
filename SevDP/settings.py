2f98fb6d189bf399e5300d31e1fc39046e1dd78b
Alexei Doloban <alekdn.work@yandex.ru>
2024-01-31 23:40:31 +0300

    readyFinal13

--- SevDP/settings.py
+++ SevDP/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'media_root')
 
 
 
