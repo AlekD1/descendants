771cd3f33e8fd0312b561eb9b7a02d223e2890d1
Alexei Doloban <alekdn.work@yandex.ru>
2024-02-01 00:29:57 +0300

    readyFinal20

--- home/models.py
+++ home/models.py
@@ -10,7 +10,7 @@ class Gallery(models.Model):
     image = models.ImageField(
         verbose_name='Изображение',
         max_length=255,
-        upload_to=MEDIA_ROOT/'gallery'
+        upload_to="static/gallery"
     )
 
 
