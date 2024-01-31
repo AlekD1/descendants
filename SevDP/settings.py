75e12daa928445631cb7e47d3ebb40ac14764d91
Alexei Doloban <alekdn.work@yandex.ru>
2024-01-31 14:58:32 +0300

    ready6

--- SevDP/settings.py
+++ SevDP/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'static_files'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
